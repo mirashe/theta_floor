ced92147caa8d48e459780dc5dd09bdedaf7fdf0
Nabi Mirashe <n.mirashe@gmail.com>
2023-07-07 01:02:20 +0200

    use decimal

--- tetha.py
+++ tetha.py
@@ -3,42 +3,46 @@ import math
 from decimal import Decimal, getcontext
 
 def is_prime(number):
-    if number < 2:
+    if number < Decimal(2):
         return False
 
-    sqrt_num = Decimal(number).sqrt()
-    sqrt_num = sqrt_num.to_integral_value(context=getcontext().prec)
+    sqrt_num = number.sqrt()
+    sqrt_num = sqrt_num.to_integral_value()
 
-    for i in range(2, sqrt_num + 1):
-        if number % i == 0:
+    for i in range(Decimal(2), sqrt_num + Decimal(1)):
+        if number % i == Decimal(0):
             return False
 
     return True
 
 def next_prime(number):
-    if number < 2:
-        return 2
+    if number < Decimal(2):
+        return Decimal(2)
 
-    next_num = number + 1
+    next_num = number + Decimal(1)
     while True:
         if is_prime(next_num):
             return next_num
-        next_num += 1
+        next_num += Decimal(1)
 
-def check_tetha(tetha) -> int:
-    n = 1
+def check_tetha(tetha):
+    n = Decimal(1)
     while True:
-        if n % 10 == 0:
+        if n % Decimal(10) == Decimal(0):
             print(f"check_tetha: checking {n}")
-        if not is_prime(math.floor(Decimal(tetha) ** n)):
+        if not is_prime(Decimal(math.floor(tetha) ** n)):
             print(f"check_tetha: check failed at {n}")
             return n
-        n += 1
+        n += Decimal(1)
 
-tetha = 2
+tetha = Decimal(2)
 
 while True:
     failed_n = check_tetha(tetha)
-    last_generated_number = math.floor(Decimal(tetha) ** failed_n)
-    next_prime = next_prime(last_generated_number)
-    tetha = 
+    last_generated_number = Decimal(math.floor(tetha) ** failed_n)
+    print(f"last_generated_number: {last_generated_number}")
+    next_prime_number = next_prime(last_generated_number)
+    print(f"next_prime_number: {next_prime_number}")
+    tetha = Decimal(next_prime_number**(1/failed_n))
+    print(f"tetha: {tetha}")
+    
