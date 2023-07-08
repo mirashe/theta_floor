7e4d6596865289e44af53ef43ef241d7620a405d
Nabi Mirashe <n.mirashe@gmail.com>
2023-07-08 11:14:27 +0200

    Fixing hash set for prime check

--- tetha.py
+++ tetha.py
@@ -12,26 +12,36 @@ def is_prime(number):
     if number < Decimal(2):
         return False
     
-    if number < max_prime:
+    if number <= max_prime:
         return number in primes
 
     sqrt_num = number.sqrt()
-    sqrt_num = sqrt_num.to_integral_value()
-    if sqrt_num < max_prime:
-        for i in primes_list:
-            if i > sqrt_num:
-                break
-            if number % i == Decimal(0):
-                return False
-    else:
-        for i in range(2, int(Decimal(number).sqrt()) + 1):
-            if number % i == Decimal(0):
-                return False
 
-    max_prime = number
-    primes.add(number)
-    primes_list.append(number)
-    return True
+    for i in primes_list:
+        if i > sqrt_num:
+            break
+        if number % i == Decimal(0):
+            return False
+
+    if sqrt_num < max_prime:
+        return True
+
+    i = Decimal(max_prime + 2)
+    if i == 4:
+        i = Decimal(3)
+
+    while True:
+        if i > sqrt_num:
+            return True
+        if is_prime(i):
+            max_prime = i
+            primes.add(i)
+            primes_list.append(i)
+
+            if number % i == Decimal(0):
+                return False
+        i += 2
+
 
 def next_prime(number):
     if number < Decimal(2):
@@ -63,3 +73,19 @@ while True:
     print(f"next_prime_number: {next_prime_number}")
     tetha = (Decimal(next_prime_number), Decimal(failed_n))
     print(f"tetha: {tetha} -> {tetha[0] ** (Decimal(1) / tetha[1])}")
+
+def check_num(num):
+    print(f"{num} is {is_prime(Decimal(num))}")
+
+def test_is_prime():
+    check_num(2)
+    check_num(3)
+    check_num(4)
+    check_num(30)
+    check_num(73)
+    check_num(24)
+    check_num(19)
+    check_num(6)
+    check_num(25)
+    check_num(73*73)
+    check_num(1)
