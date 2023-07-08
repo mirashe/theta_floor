787f8fdfa288316d2b0532945c00859161d7d0b5
Nabi Mirashe <n.mirashe@gmail.com>
2023-07-08 23:33:16 +0200

    optimize prime checking

--- tetha.py
+++ tetha.py
@@ -2,80 +2,93 @@ from ast import Tuple
 import math
 from decimal import Decimal, getcontext
 
-primes = set([2])
-primes_list = [2]
-max_prime = Decimal(2)
+Dec_0 = Decimal(0)
+Dec_1 = Decimal(1)
+Dec_2 = Decimal(2)
+Dec_3 = Decimal(3)
+Dec_4 = Decimal(4)
+Dec_10 = Decimal(10)
+
+max_prime = Dec_2
+primes_set = {max_prime}
+primes_list = [max_prime]
+
+def decimal_floor(number):
+    return number - number % 1
 
 def is_prime(number):
     global max_prime
 
-    if number < Decimal(2):
+    if number < Dec_2:
         return False
     
     if number <= max_prime:
-        return number in primes
+        return number in primes_set
 
     sqrt_num = number.sqrt()
+    sqrt_num = sqrt_num.to_integral_value()
 
     for i in primes_list:
         if i > sqrt_num:
             break
-        if number % i == Decimal(0):
+        if number % i == Dec_0:
             return False
 
     if sqrt_num < max_prime:
         return True
 
-    i = Decimal(max_prime + 2)
-    if i == 4:
-        i = Decimal(3)
+    i = max_prime + Dec_2
+    if i == Dec_4:
+        i = Dec_3
 
     while True:
         if i > sqrt_num:
             return True
         if is_prime(i):
             max_prime = i
-            primes.add(i)
+            primes_set.add(i)
             primes_list.append(i)
 
-            if number % i == Decimal(0):
+            if number % i == Dec_0:
                 return False
         i += 2
 
 
 def next_prime(number):
-    if number < Decimal(2):
-        return Decimal(2)
+    if number < Dec_2:
+        return Dec_2
 
-    next_num = number + Decimal(1)
+    next_num = number + Dec_1
     while True:
         if is_prime(next_num):
             return next_num
-        next_num += Decimal(1)
+        next_num += Dec_1
 
 def check_tetha(tetha):
-    n = Decimal(1)
+    n = Dec_1
     while True:
-        if n % Decimal(10) == Decimal(0):
+        if n % Dec_10 == Dec_0:
             print(f"check_tetha: checking {n}")
-        if not is_prime(Decimal(math.floor(tetha[0] ** (Decimal(n) / tetha[1])))):
+
+        if not is_prime(decimal_floor(tetha[0] ** (n / tetha[1]))):
             print(f"check_tetha: check failed at {n}")
             return n
-        n += Decimal(1)
+        n += Dec_1
 
-tetha = (Decimal(2), Decimal(1))
+tetha = (Dec_2, Dec_1)
 
 while True:
     failed_n = check_tetha(tetha)
-    last_generated_number = Decimal(math.floor(tetha[0] ** (Decimal(failed_n) / tetha[1])))
-    print(f"last_generated_number: {last_generated_number}")
+    last_generated_number = decimal_floor(tetha[0] ** (failed_n / tetha[1]))
+    print(f"last_generated_number: {last_generated_number:,}")
+    print(f"max prime: {max_prime:,}")
     next_prime_number = next_prime(last_generated_number)
-    print(f"next_prime_number: {next_prime_number}")
-    tetha = (Decimal(next_prime_number), Decimal(failed_n))
-    print(f"tetha: {tetha} -> {tetha[0] ** (Decimal(1) / tetha[1])}")
+    print(f"next_prime_number: {next_prime_number:,}")
+    tetha = (next_prime_number, failed_n)
+    print(f"tetha: {tetha} -> {tetha[0] ** (Dec_1 / tetha[1]):,}")
 
 def check_num(num):
-    print(f"{num} is {is_prime(Decimal(num))}")
+    print(f"{num:,} is {is_prime(Decimal(num)):,}")
 
 def test_is_prime():
     check_num(2)
