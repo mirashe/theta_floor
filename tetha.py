a0445b62c5411e6853efdb2b73fef00633454efe
Nabi Mirashe <n.mirashe@gmail.com>
2023-07-12 00:58:43 +0200

    Implemeting the mechansim of storing and retreiving prime numbers list.

--- tetha.py
+++ tetha.py
@@ -1,6 +1,7 @@
-from ast import Tuple
-import math
+import os
 from decimal import Decimal, getcontext
+
+prime_numbers_file_path = 'prime_numbers.txt'
 
 DEC_0 = Decimal(0)
 DEC_1 = Decimal(1)
@@ -10,10 +11,20 @@ DEC_4 = Decimal(4)
 DEC_10 = Decimal(10)
 DEC_1000 = Decimal(1000)
 
-max_prime = DEC_2
-primes_set = {max_prime}
-primes_list = [max_prime]
-max_distance = DEC_1 # Maximum observed distance between two consecutive prime numbers
+def read_decimals_from_file():
+    if not os.path.isfile(prime_numbers_file_path):
+        return []
+
+    decimal_list = []
+    with open(prime_numbers_file_path, 'r') as f:
+        for line in f:
+            decimal_list.append(Decimal(line.strip()))
+
+    return decimal_list
+
+def write_to_file(value):
+    with open(prime_numbers_file_path, 'a') as f:
+        f.write(str(value) + '\n')
 
 def decimal_floor(number):
     return number - number % 1
@@ -53,11 +64,11 @@ def is_prime(number):
             max_prime = i
             primes_set.add(i)
             primes_list.append(i)
+            write_to_file(i)
 
             if number % i == DEC_0:
                 return False
         i += 2
-
 
 def next_prime(number):
     if number < DEC_2:
@@ -80,20 +91,34 @@ def check_tetha(tetha):
             return n
         n += DEC_1
 
+primes_list = read_decimals_from_file()
+if len(primes_list) == 0:
+    primes_list = [DEC_2]
+    write_to_file(DEC_2)
+
+max_distance = DEC_1  # Maximum observed distance between two consecutive prime numbers
+max_prime = primes_list[-1]
+primes_set = set(primes_list)
+
 tetha = (DEC_2, DEC_1)
+check_index = 0
 
 while True:
     failed_n = check_tetha(tetha)
     last_generated_number = decimal_floor(tetha[0] ** (failed_n / tetha[1]))
+
+    print(f"check index: {check_index:,}")
     print(f"last_generated_number: {last_generated_number:,}")
     print(f"max prime: {max_prime:,}")
     print(f"max distance: {max_distance:,}")
     print(f"primes count: {len(primes_list):,}")
+    print("----------------------------------------------------")
 
     next_prime_number = next_prime(last_generated_number)
     print(f"next_prime_number: {next_prime_number:,}")
     tetha = (next_prime_number, failed_n)
     print(f"tetha: {tetha} -> {tetha[0] ** (DEC_1 / tetha[1]):,}")
+    check_index += 1
 
 def check_num(num):
     print(f"{num:,} is {is_prime(Decimal(num)):,}")
